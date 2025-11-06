be38e1589d3035dcf9c70d3be9bf6a1189a892e3
danielSenne <danielflausinosenne123@gmail.com>
2025-11-06 11:53:23 -0300

    Refatorando o código

--- projeto1/funcoes.js
+++ projeto1/funcoes.js
@@ -1,41 +1,54 @@
-const { rejects } = require('assert')
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs').promises
+const path = require('path');
 
-function lerDiretorio(caminho){
-    return new Promise((resolve, reject) => {
-        try{
-            let arquivos = fs.readdirSync(caminho)
-            arquivos.map(arquivo => path.join(caminho, arquivo))
-            resolve(arquivos)
-        }catch(e){
-            reject
-        }
-    })
+async function lerDiretorio(caminho) {
+  try {
+    const arquivos = await fs.readdir(caminho); 
+    const arquivosCompletos = arquivos.map(arquivo => path.join(caminho, arquivo));
+    return arquivosCompletos;
+  } catch (e) {
+    throw e;
+  }
 }
 
-function lerArquivo(caminho){
-    return new Promise((resolve, reject) => {
-        try{
-            const conteudo = fs.readFile(caminho, {encoding: 'utf-8'})
-            resolve(conteudo.toString())
-        }catch(e){
-            reject(e)
-        }
-    })
+async function lerArquivo(caminho) {
+  try {
+    const conteudo = await fs.readFile(caminho, 'utf-8');
+    return conteudo.toString();
+  } catch (e) {
+    throw e;
+  }
 }
 
-function lerArquivos(caminhos){
-    return Promise.all(caminhos.map(caminho => lerArquivo(caminho)))
+function lerArquivos(caminhos) {
+  return Promise.all(caminhos.map(caminho => lerArquivo(caminho)));
 }
 
-function elementosTerminadosCom(arry, padrao){
-    return arry.filter(el => el.endsWith(padrao))
+function elementosTerminadosCom(array, padrao) {
+  return array.filter(el => el.endsWith(padrao));
+}
+
+function removerSeVazio(array) {
+  return array.filter(el => el.trim());
+}
+
+function removerSeIncluir(array, padraoTextual) {
+  return array.filter(el => !el.includes(padraoTextual));
+}
+
+function removerSeApenasNumeros(array) {
+  return array.filter(el => {
+    const num = parseInt(el.trim());
+    return isNaN(num);
+  });
 }
 
 module.exports = {
-    lerDiretorio, 
-    lerArquivo,
-    lerArquivos,
-    elementosTerminadosCom
-} 
+  lerDiretorio,
+  lerArquivo,
+  lerArquivos,
+  elementosTerminadosCom,
+  removerSeVazio,
+  removerSeIncluir,
+  removerSeApenasNumeros
+};
