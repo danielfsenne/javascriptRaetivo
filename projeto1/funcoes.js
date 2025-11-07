f0e1eced2eb320607540d314ee91fe99832b22fd
danielSenne <danielflausinosenne123@gmail.com>
2025-11-07 12:33:01 -0300

    Finalização do projeto de oredenção e repetição das palavras

--- projeto1/funcoes.js
+++ projeto1/funcoes.js
@@ -1,67 +1,118 @@
-const fs = require('fs').promises
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 
-async function lerDiretorio(caminho) {
-  try {
-    const arquivos = await fs.readdir(caminho); 
-    const arquivosCompletos = arquivos.map(arquivo => path.join(caminho, arquivo));
-    return arquivosCompletos;
-  } catch (e) {
-    throw e;
-  }
+function composicao(...fns) {
+    return function(valor) {
+        return fns.reduce(async (acc, fn) => {
+            if(Promise.resolve(acc) === acc) {
+                return fn(await acc)
+            } else {
+                return fn(acc)
+            }
+        }, valor)
+    }
 }
 
-async function lerArquivo(caminho) {
-  try {
-    const conteudo = await fs.readFile(caminho, 'utf-8');
-    return conteudo.toString();
-  } catch (e) {
-    throw e;
-  }
+function lerDiretorio(caminho) {
+    return new Promise((resolve, reject) => {
+        try {
+            const arquivos = fs.readdirSync(caminho).map(arquivo => {
+                return path.join(caminho, arquivo)
+            })
+            resolve(arquivos)
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+function lerArquivo(caminho) {
+    return new Promise((resolve, reject) => {
+        try {
+            const conteudo = fs.readFileSync(caminho, { encoding: 'utf-8' })
+            resolve(conteudo.toString())
+        } catch (e) {
+            reject(e)
+        }
+    })
 }
 
 function lerArquivos(caminhos) {
-  return Promise.all(caminhos.map(caminho => lerArquivo(caminho)));
+    return Promise.all(caminhos.map(caminho => lerArquivo(caminho)))
 }
 
-function elementosTerminadosCom(array, padrao) {
-  return array.filter(el => el.endsWith(padrao));
+function elementosTerminadosCom(padraoTextual) {
+    return function (array) {
+        return array.filter(el => el.endsWith(padraoTextual))
+    }
 }
 
-function removerSeVazio(array) {
-  return array.filter(el => el.trim());
+function removerElementosSeVazio(array) {
+    return array.filter(el => el.trim())
 }
 
-function removerSeIncluir(array, padraoTextual) {
-  return array.filter(el => !el.includes(padraoTextual));
+function removerElementosSeIncluir(padraoTextual) {
+    return function (array) {
+        return array.filter(el => !el.includes(padraoTextual))
+    }
 }
 
-function removerSimbolos(simbolos){
-    return function(array){
+function removerElementosSeApenasNumero(array) {
+    return array.filter(el => {
+        const num = parseInt(el.trim())
+        return num !== num
+    })
+}
+
+function removerSimbolos(simbolos) {
+    return function (array) {
         return array.map(el => {
-            let textoSemSimbolos = el
-            simbolos.forEach(simbolo => {
-                textoSemSimbolos = textoSemSimbolos.split(simbolo).join('')
-            })
-            return textoSemSimbolos
+            return simbolos.reduce((acc, simbolo) => {
+                return acc.split(simbolo).join('')
+            }, el)
         })
     }
 }
 
-function removerSeApenasNumeros(array) {
-  return array.filter(el => {
-    const num = parseInt(el.trim());
-    return isNaN(num);
-  });
+function mesclarElementos(array) {
+    return array.join(' ')
+}
+
+function separarTextoPor(simbolo) {
+    return function (texto) {
+        return texto.split(simbolo)
+    }
+}
+
+function agruparElementos(palavras) {
+    return Object.values(palavras.reduce((acc, palavra) => {
+        const el = palavra.toLowerCase()
+        const qtde = acc[el] ? acc[el].qtde + 1 : 1
+        acc[el] = { elemento: el, qtde }
+        return acc
+    }, {}))
+}
+
+function ordernarPorAtribNumerico(attr, ordem = 'asc') {
+    return function (array) {
+        const asc = (o1, o2) => o1[attr] - o2[attr]
+        const desc = (o1, o2) => o2[attr] - o1[attr]
+        return [...array].sort(ordem === 'asc' ? asc : desc)
+    }
 }
 
 module.exports = {
-  lerDiretorio,
-  lerArquivo,
-  lerArquivos,
-  elementosTerminadosCom,
-  removerSeVazio,
-  removerSeIncluir,
-  removerSeApenasNumeros,
-  removerSimbolos
-};
+    composicao,
+    lerDiretorio,
+    lerArquivo,
+    lerArquivos,
+    elementosTerminadosCom,
+    removerElementosSeVazio,
+    removerElementosSeIncluir,
+    removerElementosSeApenasNumero,
+    removerSimbolos,
+    mesclarElementos,
+    separarTextoPor,
+    agruparElementos,
+    ordernarPorAtribNumerico
+}
--- projeto1/index.js
+++ projeto1/index.js
@@ -1,19 +1,27 @@
-const fn = require('./funcoes')
 const path = require('path')
+const fn = require('./funcoes')
 
 const caminho = path.join(__dirname, '..', 'dados', 'legendas')
 
 const simbolos = [
-    '.', '?', '-', ',', '"','_', '<i>', '</i>', '\r', '[]', '(',')'
+    '.', '?', '-', ',', '"', '♪',
+    '_', '<i>', '</i>', '\r', '[', ']',
+    '(', ')'
 ]
 
 fn.lerDiretorio(caminho)
-    .then(arquivos => fn.elementosTerminadosCom(arquivos, '.srt'))
-    .then(arquivosSRT => fn.lerArquivos(arquivosSRT))
-    .then(conteudos => conteudos.join('\n'))
-    .then(todoConteudo => todoConteudo.split('\n'))
-    .then(linhas => fn.removerSeVazio(linhas))
-    .then(linhas => fn.removerSeIncluir(linhas, '-->'))
-    .then(linhas => fn.removerseApenasNumeros(linhas))
+    .then(fn.elementosTerminadosCom('.srt'))
+    .then(fn.lerArquivos)
+    .then(fn.mesclarElementos)
+    .then(fn.separarTextoPor('\n'))
+    .then(fn.removerElementosSeVazio)
+    .then(fn.removerElementosSeIncluir('-->'))
+    .then(fn.removerElementosSeApenasNumero)
     .then(fn.removerSimbolos(simbolos))
-    .then(console.log) 
+    .then(fn.mesclarElementos)
+    .then(fn.separarTextoPor(' '))
+    .then(fn.removerElementosSeVazio)
+    .then(fn.removerElementosSeApenasNumero)
+    .then(fn.agruparElementos)
+    .then(fn.ordernarPorAtribNumerico('qtde', 'desc'))
+    .then(console.log)
